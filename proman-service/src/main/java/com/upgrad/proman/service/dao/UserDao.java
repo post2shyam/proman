c5dc8976be9721abacba787373e7bab8261a54b5
Shyam Sunder Grover <post2shyam@gmail.com>
2020-11-18 22:15:08 +0530

    Exception handling coded added
    Exception handling coded added

--- proman-service/src/main/java/com/upgrad/proman/service/dao/UserDao.java
+++ proman-service/src/main/java/com/upgrad/proman/service/dao/UserDao.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 
 @Repository
@@ -18,6 +19,12 @@ public class UserDao {
     }
 
     public UserEntity getUser(final String userUuid) {
-        return entityManager.createNamedQuery("userByUuid", UserEntity.class).setParameter("uuid", userUuid).getSingleResult();
+        UserEntity singleResult = null;
+        try{
+            singleResult = entityManager.createNamedQuery("userByUuid", UserEntity.class).setParameter("uuid", userUuid).getSingleResult();
+        }catch (NoResultException nre){
+            //Do nothing
+        }
+        return singleResult;
     }
 }
